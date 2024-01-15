0f2f4aaad2676d5d30b3fc9c58c6726672876df6
Robert Kaussow <mail@thegeeklab.de>
2024-01-15 21:19:32 +0100

    fix: use selector loop to manually render all charts on a page

--- src/js/mermaid.js
+++ src/js/mermaid.js
@@ -1,30 +1,39 @@
 const Storage = require("store2")
+const { v4: uuidv4 } = require("uuid")
 const { COLOR_THEME_DARK, THEME, COLOR_THEME_AUTO } = require("./config.js")
+
+import mermaid from "mermaid"
 
 document.addEventListener("DOMContentLoaded", function (event) {
   let lstore = Storage.namespace(THEME)
-  let currentColorTheme = lstore.get("color-theme") || COLOR_THEME_AUTO
+  let currentMode = lstore.get("color-theme") || COLOR_THEME_AUTO
   let darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)")
   let darkMode = false
   let theme = "default"
 
   if (
-    currentColorTheme === COLOR_THEME_DARK ||
-    (currentColorTheme === COLOR_THEME_AUTO && darkModeQuery.matches)
+    currentMode === COLOR_THEME_DARK ||
+    (currentMode === COLOR_THEME_AUTO && darkModeQuery.matches)
   ) {
     darkMode = true
     theme = "dark"
   }
 
-  import("mermaid")
-    .then(({ default: md }) => {
-      md.initialize({
-        flowchart: { useMaxWidth: true },
-        theme: theme,
-        themeVariables: {
-          darkMode: darkMode
-        }
-      })
+  mermaid.initialize({
+    startOnLoad: false,
+    flowchart: { useMaxWidth: true },
+    theme: theme,
+    themeVariables: {
+      darkMode: darkMode
+    }
+  })
+
+  document.querySelectorAll(".mermaid").forEach(function (el) {
+    let id = "graph-" + uuidv4()
+
+    mermaid.render(id, el.innerText).then(({ svg, bindFunctions }) => {
+      el.innerHTML = svg
+      bindFunctions?.(el)
     })
-    .catch((error) => console.error(error))
+  })
 })
